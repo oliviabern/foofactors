738b23f9b0607e8f723b0760f0d1ba7c95361924
oliviabern <olivia.m.bernstein@gmail.com>
2021-02-02 16:59:19 -0800

    Added tests

--- tests/testthat/test-fbind.R
+++ tests/testthat/test-fbind.R
@@ -1,3 +1,17 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("fbind() binds factor (or character)", {
+  x <- c("a", "b")
+  x_fact <- factor(x)
+  y <- c("c", "d")
+  z <- factor(c("a", "b", "c", "d"))
+
+  expect_identical(fbind(x, y), z)
+  expect_identical(fbind(x_fact, y), z)
+})
+
+test_that("fbind() binds integers", {
+  x <- 1:2
+  y <- 3:4
+  z <- factor(1:4)
+
+  expect_identical(fbind(x,y),z)
 })
